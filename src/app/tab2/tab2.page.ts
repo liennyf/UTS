4f961f00904e1f45fdd9e2028e0294f2ca5fe660
brownies <c14180008@john.petra.ac.id>
2021-04-13 12:57:29 +0700

    C14180008-Tab2

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,4 +1,6 @@
 import { Component, Injectable } from '@angular/core';
+import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
 import { GlobalvarService, Note } from '../globalvar.service';
 import { Tab1Page } from '../tab1/tab1.page';
 
@@ -11,12 +13,14 @@ import { Tab1Page } from '../tab1/tab1.page';
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
+
 export class Tab2Page {
   isinote: Array<Note>;
+  isiData : Observable<Note[]>;
+  isiDataColl:AngularFirestoreCollection<Note>;
 
-  constructor(public page1:Tab1Page) {
-    this.isinote=this.page1.dataNote;
-    console.log(this.isinote);
+  constructor(afs : AngularFirestore) {
+    this.isiDataColl = afs.collection('note');
+    this.isiData = this.isiDataColl.valueChanges();
   }
-
 }
--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { AngularFirestoreCollection, AngularFirestore } from '@angular/fire/firestore';
+import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
+import { Note, GlobalvarService } from '../globalvar.service';
 
 @Component({
   selector: 'app-tab3',
@@ -6,7 +10,54 @@ import { Component } from '@angular/core';
   styleUrls: ['tab3.page.scss']
 })
 export class Tab3Page {
+  isiData : Note;
+  isiDataColl:AngularFirestoreCollection<Note>;
 
-  constructor() {}
+  Judul : string;
+  Isi : string;
+  Tanggal: string;
+  Nilai: number;
+  FileName: string;
+  dataNote: Note[]=[];
+  paramindex : string;
 
+  constructor(private route : ActivatedRoute, public globalService:GlobalvarService, afs : AngularFirestore) {
+  }
+
+  ngOnInit(afs : AngularFirestore) {
+    let isiindex = this.route.snapshot.paramMap.get('nama');
+    this.paramindex = isiindex;
+    console.log(this.paramindex);
+
+    if(this.paramindex==""||this.paramindex==null){
+    }
+    else{
+      this.isiDataColl = afs.collection('note/{{paramindex}}');
+      this.Judul = this.isiDataColl[0].judul;
+      console.log(this.Judul);
+    }
+  }
+
+  TambahFoto(){
+    this.globalService.tambahfoto();
+  }
+
+  update(){
+    this.FileName=this.globalService.getFn();
+
+    this.isiDataColl.doc(this.Judul).set({
+      judul : this.Judul,
+      isi : this.Isi,
+      tanggal : this.Tanggal,
+      nilai: this.Nilai,
+      fileName: this.FileName
+    });
+  }
+
+  delete(afs : AngularFirestore){
+    afs
+       .collection("note")
+       .doc(this.paramindex)
+       .delete();
+  }
 }
